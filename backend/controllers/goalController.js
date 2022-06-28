ac7f8f15030250f6a9d7d251001300cb5a13efe2
Jetsuko <jessean.infiltrator@gmail.com>
2022-06-27 19:21:04 -0700

    Goals controller and routes setup redo

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -8,7 +8,7 @@ const getGoals = asyncHandler(async (req, res) => {
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
-const asyncHandler(setGoal = async (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field');
