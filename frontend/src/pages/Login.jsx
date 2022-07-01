67f5f4d22a74ac62a9f922e291ff026ce3a391e5
Jetsuko <jessean.infiltrator@gmail.com>
2022-06-30 17:25:29 -0700

    restarting frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,24 +4,21 @@ import {FaSignInAlt} from 'react-icons/fa';
 function Login() {
     const [formData, setFormData] = useState({
         email: '',
-        password: '',
+        password: ''
     });
-    
-    const {
-        email,
-        password,
-    } = formData;
+
+    const {email, password} = formData;
 
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
             [e.target.name]: e.target.value
         }));
-    }
+    };
 
     const onSubmit = (e) => {
         e.preventDefault();
-    }
+    };
 
     return (
         <>
@@ -31,34 +28,17 @@ function Login() {
                 </h1>
                 <p>Login and set your goals</p>
             </section>
+
             <section className="form">
                 <form onSubmit={onSubmit}>
                     <div className="form-group">
-                        <input 
-                            type="email"
-                            className="form-control"
-                            id="email"
-                            name="email"
-                            value={email}
-                            placeholder="Enter your email"
-                            onChange={onChange}
-                        />
+                        <input type="email" className="form-control" id="email" name="email" value={email} placeholder='Enter your email' onChange={onChange} />
                     </div>
                     <div className="form-group">
-                        <input 
-                            type="password"
-                            className="form-control"
-                            id="password"
-                            name="password"
-                            value={password}
-                            placeholder="Enter your password"
-                            onChange={onChange}
-                        />
+                        <input type="password" className="form-control" id="password" name="password" value={password} placeholder='Enter your password' onChange={onChange} />
                     </div>
                     <div className="form-group">
-                        <button type="submit" className="btn btn-block">
-                            Submit
-                        </button>
+                        <button type="submit" className="btn btn-block">Submit</button>
                     </div>
                 </form>
             </section>
@@ -66,4 +46,4 @@ function Login() {
     )
 }
 
-export default Login;
+export default Login
